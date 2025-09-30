73fb26727c1a880d476720677da162580927407a
Vitachan <165843769+irfadillah30@users.noreply.github.com>
2025-10-01 03:35:07 +0900

    ok

--- kontak.py
+++ kontak.py
@@ -7,6 +7,6 @@ def kontak():
     
     - **Email:** [irfadillahafninurvita@gmail.com](irfadillahafninurvita@gmail.com)
     - **LinkedIn:** [Profil LinkedIn Saya](https://www.linkedin.com/in/irfadillahafninurvita05/)
-    - **GitHub:** [Profil GitHub Saya](https://github.com/irfadillah30/BukitVista)
+    - **GitHub:** [Profil GitHub Saya](https://github.com/irfadillah30/PortofolioDataScience)
     - **Whatsapp:** [0821-2397-0898](https://wa.me/6281239708989)
     """)
