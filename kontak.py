23d99c51e5c0886f600985b930e5c98ad243ac13
Vitachan <165843769+irfadillah30@users.noreply.github.com>
2025-10-01 03:41:51 +0900

    up

--- kontak.py
+++ kontak.py
@@ -2,27 +2,46 @@ def kontak():
     import streamlit as st
 
     st.title("Contact Page")
+    st.write("If you are interested you can contact me through the following channels:")
 
-    st.write("You can reach me through the following channels:")
-
-    # Link dengan logo
     st.markdown("""
-    <table>
+    <style>
+        .contact-table td {
+            text-align: center;
+            padding: 15px;
+        }
+        .contact-text {
+            margin-top: 5px;
+            font-weight: bold;
+        }
+    </style>
+
+    <table class="contact-table">
         <tr>
-            <td><a href="mailto:irfadillahafninurvita@gmail.com" target="_blank">
-                <img src="https://img.icons8.com/color/48/000000/gmail.png"/>
-            </a></td>
-            <td><a href="https://www.linkedin.com/in/irfadillahafninurvita05/" target="_blank">
-                <img src="https://img.icons8.com/color/48/000000/linkedin.png"/>
-            </a></td>
-            <td><a href="https://github.com/irfadillah30/PortofolioDataScience" target="_blank">
-                <img src="https://img.icons8.com/ios-glyphs/48/000000/github.png"/>
-            </a></td>
-            <td><a href="https://wa.me/6281239708989" target="_blank">
-                <img src="https://img.icons8.com/color/48/000000/whatsapp.png"/>
-            </a></td>
+            <td>
+                <a href="mailto:irfadillahafninurvita@gmail.com" target="_blank">
+                    <img src="https://img.icons8.com/color/64/000000/gmail.png"/>
+                </a>
+                <div class="contact-text">Email</div>
+            </td>
+            <td>
+                <a href="https://www.linkedin.com/in/irfadillahafninurvita05/" target="_blank">
+                    <img src="https://img.icons8.com/color/64/000000/linkedin.png"/>
+                </a>
+                <div class="contact-text">LinkedIn</div>
+            </td>
+            <td>
+                <a href="https://github.com/irfadillah30/PortofolioDataScience" target="_blank">
+                    <img src="https://img.icons8.com/ios-glyphs/64/000000/github.png"/>
+                </a>
+                <div class="contact-text">GitHub</div>
+            </td>
+            <td>
+                <a href="https://wa.me/6281239708989" target="_blank">
+                    <img src="https://img.icons8.com/color/64/000000/whatsapp.png"/>
+                </a>
+                <div class="contact-text">WhatsApp</div>
+            </td>
         </tr>
     </table>
     """, unsafe_allow_html=True)
-
-    st.write("Click the icons to open the respective channel.")
